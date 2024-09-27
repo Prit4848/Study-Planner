23dd4634263bd8e4afcfeae5779089c3b30d7c8d
Rajput prit Subhashbhai <121234598+Prit4848@users.noreply.github.com>
2024-09-27 10:25:40 +0530

    Update mongoose-connection.js

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 
 // Connect to MongoDB
 mongoose
-  .connect(`${process.env.Mongodb_uri}`)
+  .connect(`${process.env.Mongodb_uri}/webapp`)
   .then(() => {
     dbgr("Connected to MongoDB");
   })
